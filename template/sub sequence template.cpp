d054359859442d308ccdb9bd26e6f2c31da286d4
Rishit Singla <ris04hit@gmail.com>
2024-08-24 13:03:23 +0530

    Added longest common subsequence function

---
+++ template/sub sequence template.cpp
@@ -0,0 +1,23 @@
+// template
+// template_subseq
+// template containing sub sequence related functions
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//
+// Length of longest common subsequence. Time Complexity: O(s1.size() * s2.size())
+template <typename T> int longest_common_subsequence(T s1, T s2){
+    vector<vector<int>> dp(s1.size()+1, vector<int>(s2.size()+1, 0));
+    for (int i = 1; i<= s1.size(); i++){
+        for (int j = 1; j<= s2.size(); j++){
+            if (s1[i-1]==s2[j-1]){
+                dp[i][j] = 1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[s1.size()][s2.size()];
+}
